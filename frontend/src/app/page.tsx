e2f964299b21e7db2926411b803b3c75a7f5be09
zachseidner1 <zachary.seidner@gmail.com>
2023-09-30 13:03:56 -0400

    Add AppIcon component

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,18 @@
+import AppIcon from "@/components/AppIcon";
 import Overview from "@/components/Overview";
+import Timeline from "@/components/Timeline";
+import { App } from "@/models/App";
 
 export default function Home() {
+  const test = {
+    name: "AppDev",
+    downIntervals: [],
+    lastUpdated: new Date(),
+    imageUrl: undefined,
+  } as App;
   return (
     <div className="bg-black flex flex-col items-center justify-center h-screen">
-      <Overview />
+      <Timeline app={test} />
     </div>
   );
 }
--- frontend/src/components/Timeline.tsx
+++ frontend/src/components/Timeline.tsx
@@ -2,18 +2,16 @@ import { DEFAULT_APP_IMG } from "@/constants";
 import { App } from "@/models/App";
 import { DownInterval } from "@/models/DownInterval";
 import React from "react";
+import AppIcon from "./AppIcon";
 
-export default function Timeline(app: App) {
+export default function Timeline({ app }: { app: App }) {
   return (
     <div className="bg-white rounded-xl flex flex-col gap-6 items-stretch p-8">
       {/* Header row */}
       <div className="flex flex-row">
-        <img
-          src={app.imageUrl ?? DEFAULT_APP_IMG}
-          className=" app-icon-shadow"
-        />
+        <AppIcon imageUrl={app.imageUrl} />
         <div className="flex flex-col">
-          <h1></h1>
+          <h3> {app.name} </h3>
         </div>
       </div>
     </div>
