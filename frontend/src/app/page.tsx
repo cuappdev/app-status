a77a736d4655bb20025e636cb17a68d13fad449a
zachseidner1 <zachary.seidner@gmail.com>
2023-09-30 17:50:25 -0400

    Move test code

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -5,15 +5,6 @@ import { App } from "@/models/App";
 import { DownInterval, Severity } from "@/models/DownInterval";
 
 export default function Home() {
-  const hoursAgo10 = new Date();
-  hoursAgo10.setHours(new Date().getHours() - 5);
-  hoursAgo10.setMinutes(new Date().getMinutes() + 1);
-  const downInterval = {
-    startTime: hoursAgo10,
-    severity: Severity.High,
-    description:
-      'Users are experiencing issues while attempting to log in to their accounts. Upon entering their credentials and clicking the "Login" button, the system fails to authenticate the user and denies access. This issue is affecting a significant number of users and is preventing them from accessing their accounts and utilizing the platform\'s services. We are aware of the issue, and our team is working to fix this bug as soon as possible.',
-  } as DownInterval;
   const test = {
     name: "AppDev",
     downIntervals: [downInterval],
@@ -28,3 +19,18 @@ export default function Home() {
     </div>
   );
 }
+const hoursAgo10 = new Date();
+hoursAgo10.setHours(new Date().getHours() - 5);
+hoursAgo10.setMinutes(new Date().getMinutes() + 1);
+const downInterval = {
+  startTime: hoursAgo10,
+  severity: Severity.High,
+  description:
+    'Users are experiencing issues while attempting to log in to their accounts. Upon entering their credentials and clicking the "Login" button, the system fails to authenticate the user and denies access. This issue is affecting a significant number of users and is preventing them from accessing their accounts and utilizing the platform\'s services. We are aware of the issue, and our team is working to fix this bug as soon as possible.',
+} as DownInterval;
+const testApp = {
+  name: "AppDev",
+  downIntervals: [downInterval],
+  lastUpdated: new Date(),
+  imageUrl: undefined,
+} as App;
