4ba39fc6e91446244e48114480bfa211e58b858f
Archit404Error <4architmehta@gmail.com>
2023-09-30 21:21:30 -0400

    Add footer

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,11 +1,12 @@
 "use client";
-import { App } from "@/models/App";
 import Overview from "@/components/Overview";
-import TimelineCard from "@/components/TimelineCard";
+import { PageFooter } from "@/components/PageFooter";
 import { ReportBug } from "@/components/ReportBug";
 import { Subscribe } from "@/components/Subscribe";
 import { Timeline } from "@/components/Timeline";
+import TimelineCard from "@/components/TimelineCard";
 import { BACKEND_URL } from "@/constants";
+import { App } from "@/models/App";
 import { useEffect, useState } from "react";
 
 interface AppsResponse {
@@ -34,7 +35,7 @@ export default function Home() {
 
   return (
     <div className="bg-gray-bug overflow-hidden">
-      <div className="flex flex-col mx-auto items-center justify-center px-4 py-8 gap-4 lg-tablet:px-6 lg-tablet:py-16 sm-desktop:py-[124px] sm-desktop:w-[1152px]">
+      <div className="flex flex-col mx-auto items-center justify-center px-4 pt-8 gap-4 lg-tablet:px-6 lg-tablet:pt-16 sm-desktop:pt-[124px] sm-desktop:w-[1152px]">
         <div className="flex flex-col mr-auto gap-2 mb-4 sm-tablet:mb-6 lg-tablet:mb-8">
           <h1 className="h2 lg-tablet:h1">Platform Status</h1>
           <p className="p1 text-gray-04">
@@ -65,6 +66,7 @@ export default function Home() {
           </div>
           <Timeline apps={apps} />
         </div>
+        <PageFooter />
       </div>
     </div>
   );
---
+++ frontend/src/components/PageFooter.tsx
@@ -0,0 +1,10 @@
+export const PageFooter = () => {
+  return (
+    <div className="mt-10 mb-4 flex flex-row">
+      <p className="p1 text-gray04">
+        Designed and developed with 🖤 in Ithaca, New York. Cornell AppDev
+        &copy; 2023
+      </p>
+    </div>
+  );
+};
