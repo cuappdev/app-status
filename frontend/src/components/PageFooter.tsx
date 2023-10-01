7a8d01e3d27a6f43538246ef7b38571b6d375402
Vin Bui <vdb23@cornell.edu>
2023-09-30 22:40:51 -0400

    Update PageFooter.tsx

--- frontend/src/components/PageFooter.tsx
+++ frontend/src/components/PageFooter.tsx
@@ -1,9 +1,9 @@
 export const PageFooter = () => {
   return (
-    <div className="mt-10 mb-4 flex flex-row">
-      <p className="p1 text-gray04">
-        Designed and developed with 🖤 in Ithaca, New York. Cornell AppDev
-        &copy; 2023
+    <div className="mt-10 mb-5 flex flex-row text-center">
+      <p className="p1 text-gray-04">
+        DESIGNED AND DEVELOPED WITH 🖤 IN ITHACA, NEW YORK. CORNELL APPDEV
+        &copy; 2023.
       </p>
     </div>
   );
