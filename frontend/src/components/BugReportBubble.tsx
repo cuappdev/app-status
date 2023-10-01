3d373b145b1f229fc73a9e8c7308f3ac193ef5c5
zachseidner1 <zachary.seidner@gmail.com>
2023-09-30 23:47:26 -0400

    Fix visibility of bug report card

--- frontend/src/components/BugReportBubble.tsx
+++ frontend/src/components/BugReportBubble.tsx
@@ -10,15 +10,23 @@ export default function BugReportBubble({
   description: string;
 }) {
   const result = resolved ? "Resolved: " : "Occurred: ";
-  return (
-    <div className="flex flex-1 bg-gray-bug border-other p-4 flex-col rounded-lg">
-      <h6>Latest bug</h6>
-      <div className="h-1" />
-      <p className="p3 sm-tablet:p1 text-gray-06">
-        {result + formatUpdateDate(lastUpdated)}
-      </p>
-      <div className="h-2" />
-      <p className="p3 sm-tablet:p1 text-gray-04">{description}</p>
-    </div>
-  );
+  // If it was update in the last 24 hours show the component
+  if (
+    new Date(lastUpdated).valueOf() >=
+    new Date().valueOf() - 3.6 * 24 * Math.pow(10, 6)
+  ) {
+    return (
+      <div className="flex flex-1 bg-gray-bug border-other p-4 flex-col rounded-lg">
+        <h6>Latest bug</h6>
+        <div className="h-1" />
+        <p className="p3 sm-tablet:p1 text-gray-06">
+          {result + formatUpdateDate(lastUpdated)}
+        </p>
+        <div className="h-2" />
+        <p className="p3 sm-tablet:p1 text-gray-04">{description}</p>
+      </div>
+    );
+  } else {
+    return <></>;
+  }
 }
