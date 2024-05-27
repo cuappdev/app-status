cb40c827f7e920468ea0a8d62153ed4b3ba4d56f
Vin Bui <vdb23@cornell.edu>
2024-05-27 18:01:49 -0400

    Add email notification for bug reports

--- backend/src/bugReports/controllers.ts
+++ backend/src/bugReports/controllers.ts
@@ -1,3 +1,4 @@
+import nodemailer from "nodemailer";
 import { BugReport, BugReportModel } from "./models";
 
 const getBugReports = () => BugReportModel.find();
@@ -11,6 +12,33 @@ const createBugReport = async (
   await BugReportModel.create(
     new BugReport(email, appName, desc, creationTime)
   );
+  sendReportEmail(email, appName, desc, creationTime).catch((err) =>
+    console.error(err)
+  );
+};
+
+const sendReportEmail = (
+  userEmail: string,
+  appName: string,
+  desc: string,
+  creationTime: Date
+) => {
+  const mailTransporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+  });
+
+  const emailDetails = {
+    from: process.env.EMAIL_USER,
+    to: process.env.EMAIL_USER,
+    subject: `[Cornell AppDev] ${appName} Bug Report`,
+    text: `Email: ${userEmail}\nReport Date: ${creationTime.toDateString()}\nApp: ${appName}\nDescription: ${desc}`,
+  };
+
+  return mailTransporter.sendMail(emailDetails);
 };
 
 export default {
